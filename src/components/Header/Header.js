292a0778568ff2be4676af1bfc15bed0c9787f33
Pragya <1993pragyapandey@gamil.com>
2023-01-21 14:06:46 +0530

    updated

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import resumeSvg from "../../assets/resume.svg";
+
+import styles from "./Header.module.css";
+
+function Header() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Header;
